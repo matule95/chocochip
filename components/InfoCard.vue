6287089dc1892d9a23db71b4bfb7e163ee77a405
Clayton Matule <clayton.matule@hotmail.com>
2019-06-02 21:31:34 +0200

    add images, tilt effect and change cursor

---
+++ components/InfoCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="info-card">
+    <b-row class="mb-5">
+      <b-col :id="`card_${index}`" md="12">
+        <img :src="src" />
+        <span class="year">2018</span>
+      </b-col>
+      <b-col md="12" class="mt-3 mb-1">
+        <h6>Model name</h6>
+      </b-col>
+      <b-col md="12" class="text-justify">
+        <p>
+          Everything is happy if you choose to make it that way. See. We take
+          the corner of the brush and let it play back-and-forth.
+        </p>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import VanillaTilt from 'vanilla-tilt'
+export default {
+  name: 'InfoCard',
+  props: {
+    src: {
+      required: true,
+      type: String
+    },
+    index: {
+      required: true,
+      type: Number
+    }
+  },
+  mounted() {
+    VanillaTilt.init(document.querySelector(`#card_${this.index}`))
+  }
+}
+</script>
+
+<style></style>
--- pages/portfolio/photography.vue
+++ pages/portfolio/photography.vue
@@ -1,9 +1,86 @@
 <template>
-  <span>Hi</span>
+  <b-container fluid>
+    <b-row>
+      <b-col md="1">
+        <filters></filters>
+      </b-col>
+      <b-col md="10" offset="1">
+        <b-row>
+          <b-col md="12">
+            <h1>
+              PHOTOGRAPHY
+            </h1>
+          </b-col>
+          <b-col md="10" class="my-3">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+              dignissim vehicula feugiat. Proin ac lacinia tellus. Etiam lorem
+              turpis, luctus sed augue nec, dictum dapibus tellus. Donec eget
+              auctor tellus. Cras fringilla, turpis id laoreet iaculis, tortor
+              magna tristique nisi, eget accumsan eros ipsum et lacus.
+            </p>
+          </b-col>
+          <b-col id="content" md="10">
+            <b-row class="mt-3">
+              <b-col
+                v-for="(image, index) of images"
+                :key="index"
+                lg="4"
+                xl="3"
+              >
+                <info-card :src="image" :index="index"></info-card>
+              </b-col>
+            </b-row>
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-export default {}
+import Filters from '~/components/Filters'
+import InfoCard from '~/components/InfoCard'
+export default {
+  components: {
+    Filters,
+    InfoCard
+  },
+  data: () => ({
+    images: [
+      '/assets/images/img-01.jpg',
+      '/assets/images/img-02.jpg',
+      '/assets/images/img-03.jpg',
+      '/assets/images/img-04.jpg',
+      '/assets/images/img-05.jpg',
+      '/assets/images/img-06.jpg',
+      '/assets/images/img-07.jpg',
+      '/assets/images/img-08.jpg',
+      '/assets/images/img-01.jpg',
+      '/assets/images/img-02.jpg',
+      '/assets/images/img-03.jpg',
+      '/assets/images/img-04.jpg',
+      '/assets/images/img-05.jpg',
+      '/assets/images/img-06.jpg',
+      '/assets/images/img-07.jpg',
+      '/assets/images/img-08.jpg'
+    ]
+  })
+}
 </script>
 
-<style></style>
+<style scoped>
+#content {
+  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none; /* IE 10+ */
+  overflow-y: scroll;
+  height: 100vh;
+  padding-bottom: 300px;
+  padding-left: 1.5rem;
+}
+#content::-webkit-scrollbar {
+  /* WebKit */
+  width: 0;
+  height: 0;
+}
+</style>
